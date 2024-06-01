08c07b74fcc813850a0c30e2094056f626cd1bca
Brownson <esitibrownson@gmail.com>
2024-06-02 00:14:08 +0100

    More changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    manifest: true,
+    // rollupOptions: {
+    //   input: '/path/to/main.js',
+    // },
+  },
   server: {
     proxy: {
       '/api': {
