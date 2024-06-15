133fe3a30953d7d75edbde233e2197e65421073c
Brownei <esitibrownson@gmail.com>
2024-06-15 01:03:50 +0100

    one more

--- src/components/Mobile/MobileInterests/MobileInterests.jsx
+++ src/components/Mobile/MobileInterests/MobileInterests.jsx
@@ -5,6 +5,7 @@ import { ClipLoader } from "react-spinners"
 import InterestsSection from "../../Chats/InterestsSection/InterestsSection"
 import { useCurrentUser } from "../../../hooks/use-current-user"
 import { pusherClient } from "../../../utils/pusherClient"
+import MessagesPage from "../../../pages/MessagesPage/MessagesPage"
 
 const MobileInterests = () => {
     const {data: user} = useCurrentUser()
@@ -36,26 +37,27 @@ const MobileInterests = () => {
 
     return (
         <main id="mobile-interests">
+            <MessagesPage />
             {isLoading ? (
                 <div className='loading'>
                     <ClipLoader fontSize={30}/>
                 </div>
             ) : error ? (
-            <div className="error">You gotta make a little refresh</div>
+                <div className="error">You gotta make a little refresh</div>
             ) : (
-            <div className="mobile-interests">
-                {friendRequests?.length > 0 ? (
-                    <div className='all-interests'>
-                        {friendRequests?.map((interest) => (
-                            <div key={interest.id}>
-                                <InterestsSection interest={interest}/>
-                            </div>
-                        ))}
-                    </div>
-                ) : (
-                    <p className="no-interests">No interests yet!</p>
-                )}
-            </div>
+                <div className="mobile-interests">
+                    {friendRequests?.length > 0 ? (
+                        <div className='all-interests'>
+                            {friendRequests?.map((interest) => (
+                                <div key={interest.id}>
+                                    <InterestsSection interest={interest}/>
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <p className="no-interests">No interests yet!</p>
+                    )}
+                </div>
             )}
         </main>
     )
