bbf171d4be3e88ae68071be2babfeba02e8d6145
brownson <esitibrownson@gmail.com>
2024-07-03 08:00:24 +0100

    Revert "Reapply "Changes""
    This reverts commit 601b0a1234abf5e93707ced7af5020cd5451d0dc.

--- src/utils/formatDate.js
+++
@@ -1,5 +0,0 @@
-export function formatDate(dateString) {
-    const options = { month: 'long', year: 'numeric' };
-    const date = new Date(dateString);
-    return `Joined ${date.toLocaleDateString('en-US', options)}`;
-}
